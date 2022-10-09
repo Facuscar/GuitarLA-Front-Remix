c79e1ffb09535c8519603d541a1c85f7dd54b776
Facuscar <facundo.scardillo@dracoder.com>
2022-10-09 18:51:47 -0300

    Finish header html, and media for the header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,11 +1,12 @@
 import { Link } from '@remix-run/react';
+import logo from '../../public/img/logo.svg';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container bar">
                 <div className="logo">
-
+                    <img src={logo} alt="GuitarLA logo" />
                 </div>
                 <nav className="nav">
                     <Link to="/">Front page</Link>
