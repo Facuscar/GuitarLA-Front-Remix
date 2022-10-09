3dd1e88bb3776baf60b061b08d215eccedfa9d38
Facuscar <facundo.scardillo@dracoder.com>
2022-10-09 19:05:18 -0300

    Add: background color when in current route

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,11 @@
-import { Link } from '@remix-run/react';
+import { Link, useLocation } from '@remix-run/react';
 import logo from '../../public/img/logo.svg';
 
 const Header = () => {
+
+    const location = useLocation();
+    console.log(location);
+
     return (
         <header className="header">
             <div className="container bar">
@@ -9,10 +13,10 @@ const Header = () => {
                     <img src={logo} alt="GuitarLA logo" />
                 </div>
                 <nav className="nav">
-                    <Link to="/">Front page</Link>
-                    <Link to="/about-us">About us</Link>
-                    <Link to="/store">Store</Link>
-                    <Link to="blog">Blog</Link>
+                    <Link to="/" className={location.pathname === '/' && 'active'}>Front page</Link>
+                    <Link to="/about-us" className={location.pathname === '/about-us' && 'active'}>About us</Link>
+                    <Link to="/store" className={location.pathname === '/store' && 'active'}>Store</Link>
+                    <Link to="blog" className={location.pathname === '/blog' && 'active'}>Blog</Link>
                 </nav>
             </div>
         </header>
