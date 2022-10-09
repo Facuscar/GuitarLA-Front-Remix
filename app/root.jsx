962e095913f4f11cb6914810abe68dcc54b7f678
Facuscar <facundo.scardillo@dracoder.com>
2022-10-09 18:22:46 -0300

    Add: initial routes

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,7 @@
 import {
     Meta,
-    Links
+    Links,
+    Outlet
 } from '@remix-run/react'
 import styles from './styles/index.css'
 
@@ -42,7 +43,7 @@ export const links = () => {
 export default function App() {
     return (
         <Document>
-            <h1>Hello, world!</h1>
+            <Outlet />
         </Document>
     );
 }
---
+++ app/routes/blog.jsx
@@ -0,0 +1,7 @@
+const Blog = () => {
+    return (
+        <h1>From Blog</h1>
+    );
+}
+
+export default Blog;
