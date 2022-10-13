a8ef1dfafe7c0eb767a2cb47d47ac568389a9961
Facuscar <facundo.scardillo@dracoder.com>
2022-10-12 23:14:29 -0300

    Add: styles to store and meta data

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,6 +1,23 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitars } from "~/models/guitars.server";
 import Guitar from "~/components/guitar";
+import styles from '~/styles/guitars.css'
+
+export const links = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ]
+}
+
+export const meta = () => {
+    return {
+        title: 'GuitarLA - Guitar store',
+        description: 'GuitarLA - Our guitar collection',
+    }
+}
 
 export const loader = async () => {
     const guitars = await getGuitars();
