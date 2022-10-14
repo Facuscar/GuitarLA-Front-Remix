9fe6934b5f4547f3970de729054c9ad2546e717f
Facuscar <facundo.scardillo@dracoder.com>
2022-10-13 22:12:47 -0300

    Add guitar info on guitar page

--- app/routes/guitar/$guitarUrl.jsx
+++ app/routes/guitar/$guitarUrl.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitar } from "~/models/guitars.server";
+import styles from '~/styles/guitars.css'
 
 export const loader = async ({ params } ) => {
     const { guitarUrl } = params;
@@ -7,10 +8,28 @@ export const loader = async ({ params } ) => {
     return await getGuitar(guitarUrl);
 }
 
+export const links = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ]
+}
+
 const Guitar = () => {
     const guitar = useLoaderData();
+    const { name, description, image, price } = guitar.data[0].attributes;
     return (
-        <h1>Hi</h1>
+        <main className="container guitar">
+            <img className="image" src={image.data.attributes.url} alt={`Guitar ${name} in a white background`} />
+
+            <div className="content">
+                <h3>{name}</h3>
+                <p className="text">{description}</p>
+                <p className="price">{price}</p>
+            </div>
+        </main>
     );
 }
 
