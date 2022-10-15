b3cccbbdecabfdcbe8a2c03ccf32e8a0a1f82be2
Facuscar <facundo.scardillo@dracoder.com>
2022-10-15 20:46:31 -0300

    Add: dinamic routing for the blog

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -2,3 +2,8 @@ export const getPosts = async () => {
     const response = await fetch(`${process.env.API_URL}/blogs?populate=image`); 
     return await response.json();
 }
+
+export const getPost = async (post) => {
+    const response = await fetch(`${process.env.API_URL}/blogs?filters[url]=${post}&populate=image`);
+    return await response.json();
+}
--- app/routes/posts/$postUrl.jsx
+++ app/routes/posts/$postUrl.jsx
@@ -1,4 +1,23 @@
+import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/posts.server";
+
+export const loader = async ({ params } ) => {
+    const { postUrl } = params;
+    const post = await getPost(postUrl);
+
+    if(post.data.length === 0) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'Guitar not found',
+        })
+    }
+
+    return post;
+}
+
 const Post = () => {
+    const post = useLoaderData();
+    console.log(post);
     return (
         <h1>This is a post</h1>
     );
