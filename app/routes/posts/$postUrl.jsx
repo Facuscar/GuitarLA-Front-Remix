911d6fbd92928ab581b97fa78c2576ad3a2c27de
Facuscar <facundo.scardillo@dracoder.com>
2022-10-15 21:08:27 -0300

    Create blog content

--- app/routes/posts/$postUrl.jsx
+++ app/routes/posts/$postUrl.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/posts.server";
+import { formatDate } from "~/utils/helpers";
+import styles from '~/styles/blog.css'
 
 export const loader = async ({ params } ) => {
     const { postUrl } = params;
@@ -8,18 +10,49 @@ export const loader = async ({ params } ) => {
     if(post.data.length === 0) {
         throw new Response('', {
             status: 404,
-            statusText: 'Guitar not found',
+            statusText: 'Post not found',
         })
     }
 
     return post;
 }
+export const meta = ({data}) => {
+    if(!data) {
+        return {
+            title: 'Post not found',
+            description: 'Post not found',
+        }
+    }
+    return {
+        title: `GuitarLA - ${data.data[0].attributes.title}`,
+        description: `Guitar blog, ${data.data[0].attributes.title}`,
+    }
+}
+
+export const links = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ]
+}
 
 const Post = () => {
     const post = useLoaderData();
-    console.log(post);
+    
+    const { title, content, image, publishedAt } = post.data[0]?.attributes;
+    console.log(title);
+
     return (
-        <h1>This is a post</h1>
+        <article className="container post">
+            <img src={image.data.attributes.formats.small.url} alt={`Blog of ${title}`} className="image"/>
+            <div className="content">
+                <h3>{title}</h3>
+                <p className="date">{formatDate(publishedAt)}</p>
+                <p className="abstract">{content}</p>
+            </div>
+        </article>
     );
 }
 
